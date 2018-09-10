869a9a2b19bffffb94eb789763cb1d386ccfda38
lakshmi-narayana <narayana.zeta@yahoo.com>
2018-09-10 14:11:23 +0530

    unit test cases

---
+++ spec/testCases.js
@@ -0,0 +1,19 @@
+//Test Cases 
+
+describe("Valid Api", function(){
+    var api;
+    var validApiUrl = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    beforeEach(function(){
+        api = validApiUrl;
+    });
+    describe("Valid API Key", function(){
+        var api_key;
+        var valid_api_key = "d508fadfdd834f57a7eb12b8fa4cc50d";
+        beforeEach(function(){
+            api_key = valid_api_key;
+        });
+        describe("Navigate News URL", function(){
+            
+        });
+});
+});
